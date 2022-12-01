112e46cf5be994dbcac7ed8078a0f8ecdaf5bc31
abidbe <abid.i.ft20@mail.umy.ac.id>
2022-12-01 11:33:17 +0700

    step 2

---
+++ src/test/java/ws/c/tugas/TugasApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.c.tugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
